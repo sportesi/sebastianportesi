5490c418118821ebec482c9908e4b8d20890a564
Sebastian Portesi <sportesi@octopus.com.ar>
2018-02-09 14:31:27 -0300

    Initial Commit

---
+++ development/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Development Login</title>
+  <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+  <script type="text/javascript" src="main.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="login-page">
+    <div class="form">
+      <form class="login-form">
+        <input type="text" placeholder="Usuario"/>
+        <input type="password" placeholder="Contraseña"/>
+        <button>Login</button>
+        <p class="message">¿No tenés cuenta? <a href="">Enterate como conseguir una</a></p>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
